207cec780e9d0553a069bbcd3cfa05c1a93e292e
Isaac Arnold <isaac@iaivphotography.com>
2020-10-02 14:10:49 -0500

    typo fixed in URL code

--- logic.js
+++ logic.js
@@ -2,7 +2,7 @@ var searchterm = "superbowl";
 var numberofresults = "";
 var startDate = "2010";
 var endDate = "2015";
-var filter = "page=0&facet=true&begin_date="+startDate+"0101&end_date="+endDate+"20180101";
+var filter = "page=0&facet=true&begin_date="+startDate+"0101&end_date="+endDate+"0101";
 var api = "AA3e1PjKzfmiQmnT8emVMNoiAx1DUAIL"; 
 var queryURL= "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchterm+"&fq="+filter+"&api-key="+api;
 $.ajax({
